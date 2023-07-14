1cb2a57eab40b75275ad8b7e8a9de8dc325fc8c8
Plēpis <ansis.plepis@if.lv>
2023-07-14 15:53:49 +0300

    Do some random changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,19 +5,17 @@ import { Montserrat } from "next/font/google";
 import { Header } from "@/components/Header";
 import "./index.css";
 import { PageBody } from "@/components/PageBody";
-import { QueryClient, QueryClientProvider } from "react-query";
 import { trpc } from "@/utils/trpc";
 import { AuthProvider } from "@/contexts/AuthContext";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
-const client = new QueryClient();
-
 const RootLayout: React.FC<PropsWithChildren> = ({ children }) => {
 	return (
 		<html>
 			<head>
-				<title>YouTube OAuth</title>
+				<title>YPI - YouTube Playlist Inspector</title>
+				<link rel="icon" type="image/x-icon" href="/favicon.ico" />
 				<meta name="description" content="YouTube OAuth Client demo" />
 			</head>
 			<body className={montserrat.className}>
